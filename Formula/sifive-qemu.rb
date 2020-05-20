610e4319e46dbabc0af0e307a24acb644664ac7a
Emmanuel Blot <emmanuel.blot@free.fr>
2020-05-20 16:16:15 +0200

    Add QEMU

---
+++ Formula/sifive-qemu.rb
@@ -0,0 +1,51 @@
+class SifiveQemu < Formula
+  desc "Emulator for SiFive RISC-V"
+  homepage "https://www.qemu.org/"
+  url "git://git.qemu.org/qemu.git", :tag => "v4.1.0"
+
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libssh"
+  depends_on "libusb"
+  depends_on "lzo"
+  depends_on "ncurses"
+  depends_on "pixman"
+  depends_on "vde"
+
+  def install
+    ENV["LIBTOOL"] = "glibtool"
+
+    args = %W[
+      --prefix=#{prefix}
+      --cc=#{ENV.cc}
+      --host-cc=#{ENV.cc}
+      --target-list=riscv32-softmmu,riscv64-softmmu
+      --disable-bsd-user
+      --disable-guest-agent
+      --enable-curses
+      --enable-libssh
+      --enable-vde
+      --extra-cflags=-DNCURSES_WIDECHAR=1
+      --enable-cocoa
+      --disable-sdl
+      --disable-gtk
+      --disable-libusb
+      --disable-vhost-user
+      --disable-vhost-kernel
+    ]
+    # Sharing Samba directories in QEMU requires the samba.org smbd which is
+    # incompatible with the macOS-provided version. This will lead to
+    # silent runtime failures, so we set it to a Homebrew path in order to
+    # obtain sensible runtime errors. This will also be compatible with
+    # Samba installations from external taps.
+    args << "--smbd=#{HOMEBREW_PREFIX}/sbin/samba-dot-org-smbd"
+
+    system "./configure", *args
+    system "make", "V=1", "install"
+  end
+end
+
